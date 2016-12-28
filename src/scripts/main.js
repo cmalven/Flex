979428607b021a494da36417855d8eec6b22a733
Chris Malven <cmalven@chrismalven.com>
2016-12-28 10:37:47 -0600

    Allows copying and pasting of clipboard contents.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,6 @@
+var Clipboard = require('clipboard');
+var notie = require('notie');
+
 //
 //   Global App Variable
 //
@@ -12,7 +15,24 @@ window.APP = window.APP || {};
 //////////////////////////////////////////////////////////////////////
 
 APP.init = function() {
- 
+  notie.setOptions({
+    colorSuccess: '#20f0bc',
+    colorError: '#fe0044'
+  })
+
+  var clipboard = new Clipboard('.js-item', {
+    text: function(trigger) {
+      return trigger.getAttribute('data-code');
+    }
+  });
+
+  clipboard.on('success', function(e) {
+    notie.alert('success', 'The code was copied to your clipboard.', 2);
+  });
+
+  clipboard.on('error', function(e) {
+    notie.alert('error', 'Your browser might not support copying to the clipboard.', 3);
+  });
 };
 
 
